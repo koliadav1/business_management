51fdbbf0037d73e2fe47a7dbcf7296a32e4434ab
Nikolay Davydov <koliadav123@gmail.com>
2026-04-13 15:23:59 +0700

    password verification added

--- src/admin/users.py
+++ src/admin/users.py
@@ -1,7 +1,10 @@
+from fastapi_users import InvalidPasswordException
 from sqladmin import ModelView
 from fastapi_users.password import PasswordHelper
+from wtforms import PasswordField, ValidationError
 
 from src.models.users import User
+from src.utils.password_validation import password_validate
 
 password_helper = PasswordHelper()
 
@@ -23,12 +26,32 @@ class UserAdmin(ModelView, model=User):
         User.updated_at,
         User.id,
     ]
+    form_overrides = {"hashed_password": PasswordField}
 
     async def on_model_change(
         self, data: dict, model: User, is_created: bool, request
     ) -> None:
-        raw_password = (
-            data.get("hashed_password") or password_helper.generate()
-        )
-        if is_created or model.hashed_password != raw_password:
+        raw_password = data.get("hashed_password")
+        email = data.get("email") or getattr(model, "email", None)
+
+        if raw_password:
+            try:
+                password_validate(raw_password, email)
+            except InvalidPasswordException as e:
+                raise ValidationError(
+                    f"Password verification failed: {e.reason}"
+                )
             data.update(hashed_password=password_helper.hash(raw_password))
+
+        elif is_created:
+            generated_password = password_helper.generate()
+            try:
+                password_validate(generated_password, email)
+            except InvalidPasswordException as e:
+                raise ValidationError(
+                    f"Password verification failed: {e.reason}"
+                )
+
+            data.update(
+                hashed_password=password_helper.hash(generated_password)
+            )
