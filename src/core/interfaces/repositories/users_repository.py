ef2d0febce6ecd86965098b324470d56bf8daf10
Nikolay Davydov <koliadav123@gmail.com>
2026-04-23 16:23:09 +0700

    changed permissions for team logic, team creation logic and added quit team logic

--- src/core/interfaces/repositories/users_repository.py
+++ src/core/interfaces/repositories/users_repository.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from src.models.users import User
+from src.models.users import User, UserRole
 
 
 class IUsersRepository(ABC):
@@ -12,4 +12,9 @@ class IUsersRepository(ABC):
     @abstractmethod
     async def exists(self, user_id: int) -> bool:
         """Проверка существования пользоватея"""
+        pass
+
+    @abstractmethod
+    async def update_user_role(self, user: User, new_role: UserRole) -> None:
+        """Изменить роль пользователя"""
         pass
--- src/services/team_service.py
+++ src/services/team_service.py
@@ -27,8 +27,6 @@ class TeamService:
         Только для роли admin
         """
         async with uow:
-            if current_user.role != UserRole.ADMIN:
-                raise ForbiddenError("Only admins can create a team")
             if current_user.team_id is not None:
                 raise UserAlreadyInTeamError("You're already in team")
 
@@ -41,7 +39,7 @@ class TeamService:
             team = Team(name=name, description=description)
             created_team = await uow.teams_repo.add(team)
 
-            user = await uow.users_repo.get(current_user.id)
+            user = await uow.session.merge(current_user)
             await uow.teams_repo.add_member(created_team, user, UserRole.ADMIN)
         return created_team
 
@@ -50,8 +48,6 @@ class TeamService:
         Получить информацию о команде
         """
         async with uow:
-            if team_id is None:
-                raise TeamNotFoundError("You have no team")
             team = await uow.teams_repo.get(team_id)
             if not team:
                 raise TeamNotFoundError(
@@ -62,56 +58,46 @@ class TeamService:
     async def get_team_members(
         self,
         uow: IUnitOfWork,
-        team_id: int,
         current_user: User,
         role: UserRole | None = None,
     ) -> List[User]:
         """
-        Получить участников команды.
+        Получить всех участников команды с фильтром по роли.
         Только для участников команды
         """
         async with uow:
-            team = await uow.teams_repo.get(team_id)
-            if not team:
-                raise TeamNotFoundError(f"Team with id {team_id} not found")
+            if current_user.team_id is None:
+                raise ForbiddenError("You're not in team")
 
-            if current_user.team_id != team_id:
-                raise ForbiddenError("You can't view team members")
-
-            members = await uow.teams_repo.get_team_members(team_id, role)
+            members = await uow.teams_repo.get_team_members(
+                current_user.team_id, role
+            )
         return members
 
     async def add_member(
         self,
         uow: IUnitOfWork,
-        team_id: int,
         user_id: int,
         current_user: User,
         role: UserRole = UserRole.EMPLOYEE,
     ) -> User:
         """
         Добавить пользователя в команду.
-        Только для admin и manager команды.
+        Только для admin команды.
         """
         async with uow:
-            if (
-                current_user.role not in [UserRole.ADMIN, UserRole.MANAGER]
-                or current_user.team_id != team_id
-            ):
-                raise ForbiddenError(
-                    "Only team admins and managers can add members"
-                )
+            if current_user.role != UserRole or current_user.team_id is None:
+                raise ForbiddenError("Only team admins can add members")
 
-            team = await uow.teams_repo.get(team_id)
+            team = await uow.teams_repo.get(current_user.team_id)
             if not team:
-                raise TeamNotFoundError(f"Team with id {team_id} not found")
+                raise TeamNotFoundError(
+                    f"Team with id {current_user.team_id} not found"
+                )
 
             user = await uow.users_repo.get(user_id)
             if not user:
                 raise UserNotFoundError(f"User with id {user_id} not found")
-
-            if user.role == UserRole.ADMIN:
-                raise ForbiddenError("Can't add admin to a team")
 
             if user.team_id is not None:
                 raise UserAlreadyInTeamError(
@@ -127,15 +113,11 @@ class TeamService:
     ) -> None:
         """
         Удаление пользователя из команды.
-        Только для admin и manager команды
+        Только для admin  команды
         """
         async with uow:
-            if current_user.team_id is None:
-                raise ForbiddenError("You're not in team")
-            if current_user.role not in [UserRole.ADMIN, UserRole.MANAGER]:
-                raise ForbiddenError(
-                    "Only team admins and managers can remove members"
-                )
+            if current_user.role != UserRole or current_user.team_id is None:
+                raise ForbiddenError("Only team admins can remove members")
 
             user = await uow.users_repo.get(user_id)
             if not user:
@@ -154,49 +136,45 @@ class TeamService:
     async def update_member_role(
         self,
         uow: IUnitOfWork,
-        team_id: int,
         user_id: int,
         new_role: UserRole,
         current_user: User,
     ) -> User:
         """
         Изменить роль участника команды.
-        Только для admin и manager команды
+        Только для admin команды
         """
         async with uow:
             if (
-                current_user.role not in [UserRole.ADMIN, UserRole.MANAGER]
-                or current_user.team_id != team_id
+                current_user.role != UserRole.ADMIN
+                or current_user.team_id is None
             ):
-                raise ForbiddenError(
-                    "Only team admins and managers can change roles"
-                )
+                raise ForbiddenError("Only team admins can change roles")
 
-            if new_role not in [UserRole.MANAGER, UserRole.EMPLOYEE]:
+            if new_role not in [
+                UserRole.MANAGER,
+                UserRole.EMPLOYEE,
+                UserRole.ADMIN,
+            ]:
                 raise InvalidRoleError(
-                    f"Invalid role: {new_role}. Must be MANAGER or EMPLOYEE"
+                    f"Invalid role: {new_role}. "
+                    "Must be MANAGER, EMPLOYEE or ADMIN"
                 )
-
-            team = await uow.teams_repo.get(team_id)
-            if not team:
-                raise TeamNotFoundError(f"Team with id {team_id} not found")
 
             user = await uow.users_repo.get(user_id)
             if not user:
                 raise UserNotFoundError(f"User with id {user_id} not found")
 
-            if user.team_id != team_id:
+            if user.team_id != current_user.team_id:
                 raise UserNotInTeamError(
-                    f"User {user_id} not in team {team_id}"
+                    f"User {user_id} not in team {current_user.team_id}"
                 )
 
-            await uow.teams_repo.update_member_role(team, user, new_role)
+            await uow.users_repo.update_user_role(user, new_role)
 
         return user
 
-    async def delete_team(
-        self, uow: IUnitOfWork, team_id: int, current_user: User
-    ) -> None:
+    async def delete_team(self, uow: IUnitOfWork, current_user: User) -> None:
         """
         Удаление команды.
         Только для admin команды
@@ -204,14 +182,22 @@ class TeamService:
         async with uow:
             if current_user.role != UserRole.ADMIN:
                 raise ForbiddenError("Only admins can delete teams")
-
-            team = await uow.teams_repo.get(team_id)
-            if not team:
-                raise TeamNotFoundError(f"Team with id {team_id} not found")
-
-            await uow.teams_repo.delete(team_id)
+            await uow.teams_repo.delete(current_user.team_id)
+            await uow.users_repo.update_user_role(current_user, UserRole.USER)
 
     async def get_all_teams(self, uow: IUnitOfWork) -> List[Team]:
         """Получить список всех команд"""
         async with uow:
             return await uow.teams_repo.get_all()
+
+    async def quit_team(self, uow: IUnitOfWork, current_user: User) -> None:
+        if current_user.role == UserRole.ADMIN:
+            admins = await uow.teams_repo.get_team_members(
+                current_user.team_id, UserRole.ADMIN
+            )
+            if any(current_user != user for user in admins):
+                await uow.teams_repo.remove_member(current_user)
+            else:
+                ForbiddenError("You must assign another admin to replace you")
+        else:
+            await uow.teams_repo.remove_member(current_user)
